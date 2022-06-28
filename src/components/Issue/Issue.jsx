ee2498b87fe7ad8ca5217d23ead130119e92fe27
Tarun Singh <tarunsingh5169202@gmail.com>
2022-06-28 12:49:38 +0530

    Update UI, add icons

--- src/components/Issue/Issue.jsx
+++ src/components/Issue/Issue.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactTooltip from 'react-tooltip';
 
 import styles from './Issue.module.css';
+import gitPullRequestIcon from '../../assets/pull_request_icon.png';
 
 const Issue = ({
   isLast,
@@ -19,6 +20,11 @@ const Issue = ({
       </div> */}
       {isLast ? (
         <div ref={setLastElement} className={styles.Issue}>
+          {'pull_request' in item ? (
+            <img src={gitPullRequestIcon} alt="icon" />
+          ) : (
+            <span>hi</span>
+          )}
           <a href={item.html_url}>{item.title}</a>
           <span>
             {item.labels.map((label) => (
@@ -47,43 +53,54 @@ const Issue = ({
           </div>
         </div>
       ) : (
-        <div key={item.id} className={styles.Issue}>
-          <a href={item.html_url}>{item.title}</a>
-          <span>
-            {item.labels.map((label) => (
-              <span
-                data-tip={label.description}
-                key={label.id}
-                style={{
-                  cursor: 'pointer',
-                  backgroundColor: `#${label.color}`,
-                }}
-                className={styles.Label}
-              >
-                {label.name}
-                <ReactTooltip
-                  place="bottom"
-                  className={styles.ToolTip}
-                  effect="solid"
-                />
-              </span>
-            ))}
-          </span>
-          <div className={styles.IssueContent}>
-            <span>#{item.number}</span>
-            <span>
-              {item.state}
-              {'ed'}
-            </span>
-            <span>
-              {formatDistanceToNow(new Date(item.created_at), {
-                addSuffix: true,
-              })}
-            </span>
-            {'by'}
-            <span>{item.user.login}</span>
+        <>
+          <div key={item.id} className={styles.Issue}>
+            <div className={styles.IssueContainer}>
+              {'pull_request' in item ? (
+                <img src={gitPullRequestIcon} alt="icon" />
+              ) : (
+                <div className={styles.OpenBug}></div>
+              )}
+              <div>
+                <a href={item.html_url}>{item.title}</a>
+                <span>
+                  {item.labels.map((label) => (
+                    <span
+                      data-tip={label.description}
+                      key={label.id}
+                      style={{
+                        cursor: 'pointer',
+                        backgroundColor: `#${label.color}`,
+                      }}
+                      className={styles.Label}
+                    >
+                      {label.name}
+                      <ReactTooltip
+                        place="bottom"
+                        className={styles.ToolTip}
+                        effect="solid"
+                      />
+                    </span>
+                  ))}
+                </span>
+                <div className={styles.IssueContent}>
+                  <span>#{item.number}</span>
+                  <span>
+                    {item.state}
+                    {'ed'}
+                  </span>
+                  <span>
+                    {formatDistanceToNow(new Date(item.created_at), {
+                      addSuffix: true,
+                    })}
+                  </span>
+                  {'by'}
+                  <span>{item.user.login}</span>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
+        </>
       )}
     </div>
   );
